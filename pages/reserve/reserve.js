cb03d98703e2d349610dececc901270cb5120267
被制作时长两年半的个人练习生 <oncwnuGD-rwPoA0lcWtWQeFAxrBo@git.weixin.qq.com>
2020-07-18 20:07:31 +0800

    1.1.5
    地理信息可以拿到了

--- pages/reserve/reserve.js
+++ pages/reserve/reserve.js
@@ -1,19 +1,20 @@
 // pages/reserve/reserve.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    photoLatitude: "",
+    photoLongitude: "",
+    photoStreet: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -31,6 +32,45 @@ Page({
         active: 1
       });
     }
+    var that = this
+    wx.getLocation({
+      //小程序要求数据格式
+      type: 'gcj02',
+      success: function (res) {
+        //console.log("请求数据了")
+        //console.log(res)
+        var latitude = res.latitude
+        var longitude = res.longitude
+        //console.log(app.qqmapsdk),
+        app.qqmapsdk.reverseGeocoder({
+          //输入坐标，返回对应文字信息
+          location: {
+            latitude: latitude,
+            longitude: longitude
+          },
+          success: function (res) {
+            //console.log("我成功了")
+            console.log(res)
+            if (res.message == 'query ok') {
+              //console.log(res)
+              var address = res.result.address
+              that.setData({
+                photoLatitude: latitude,
+                photoLongitude: longitude,
+                photoStreet: address,
+              })
+              //that.chooseImg();
+            }
+          },
+          fail: function (res) {
+            console.log("我没了，为啥呀？" + res)
+          },
+          complete: function () {
+            //console.log("我完成了，但是为啥呢？")
+          }
+        })
+      },
+    })
   },
 
   /**
