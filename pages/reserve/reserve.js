1d215d477d3f3c34f9b06b06a8fcc076807c77b0
被制作时长两年半的个人练习生 <oncwnuGD-rwPoA0lcWtWQeFAxrBo@git.weixin.qq.com>
2020-07-18 23:41:39 +0800

    1.5.6
    修复了因为用户拒绝授权造成的无法访问位置信息的情况

--- pages/reserve/reserve.js
+++ pages/reserve/reserve.js
@@ -15,13 +15,12 @@ Page({
     var that = this
     wx.getSetting({
       success(res) {
-        //console.log(res)
         if (!res.authSetting['scope.userLocation']) {
-          //console.log("没有授权位置信息"),
           wx.openSetting({
             success: function (res) {
-              //console.log(res.authSetting)
-              this.getAddress()
+              if (res.authSetting['scope.userLocation']) {
+                this.getAddress()
+              }
             }
           })
         } else {
@@ -30,76 +29,67 @@ Page({
       }
     })
   },
-
-  // getAddress: function () {
-  //   var that = this
-  //   wx.getLocation({
-  //     //小程序要求数据格式
-  //     type: 'gcj02',
-  //     success: function (res) {
-  //       //console.log("请求数据了")
-  //       console.log(res)
-  //       var latitude = res.latitude
-  //       var longitude = res.longitude
-  //       //console.log(app.qqmapsdk),
-  //       app.qqmapsdk.reverseGeocoder({
-  //         //输入坐标，返回对应文字信息
-  //         location: {
-  //           latitude: latitude,
-  //           longitude: longitude
-  //         },
-  //         success: function (res) {
-  //           //console.log("我成功了")
-  //           console.log(res)
-  //           if (res.message == 'query ok') {
-  //             //console.log(res)
-  //             var address = res.result.address
-  //             that.setData({
-  //               photoLatitude: latitude,
-  //               photoLongitude: longitude,
-  //               address: address,
-  //             })
-  //             db.collection('backdoor').where({
-  //                 _openid: wx.getStorageSync('openid')
-  //               })
-  //               .get({
-  //                 success: function (res) {
-  //                   console.log(res.data)
-  //                   if (res.data.length == 0) {
-  //                     db.collection('backdoor').add({
-  //                       data: {
-  //                         address: that.data.address,
-  //                         createTime: db.serverDate(),
-  //                         updateTime: db.serverDate()
-  //                       },
-  //                     })
-  //                   } else if (res.data[0].address != that.data.address) {
-  //                     //console.log("更新地址")
-  //                     db.collection('backdoor').where({
-  //                       _openid: wx.getStorageSync('openid')
-  //                     }).update({
-  //                       data: {
-  //                         address: that.data.address,
-  //                         updateTime: db.serverDate()
-  //                       },
-  //                     })
-  //                   }
-  //                 }
-  //               })
-  //           }
-  //         },
-  //         fail: function (res) {
-  //           console.log("我没了，为啥呀？" + res)
-  //         },
-  //         complete: function () {
-  //           //console.log("我完成了，但是为啥呢？")
-  //         }
-  //       })
-  //     },
-  //     //请求失败的话回调该函数
-  //     fail: function (res) {}
-  //   })
-  // },
+  //如果成功的话就获得用户地理位置
+  getAddress: function () {
+    var that = this
+    wx.getLocation({
+      //小程序要求数据格式
+      type: 'gcj02',
+      success: function (res) {
+        //console.log(res)
+        var latitude = res.latitude
+        var longitude = res.longitude
+        app.qqmapsdk.reverseGeocoder({
+          //输入坐标，返回对应文字信息
+          location: {
+            latitude: latitude,
+            longitude: longitude
+          },
+          success: function (res) {
+            //console.log(res)
+            if (res.message == 'query ok') {
+              var address = res.result.address
+              that.setData({
+                photoLatitude: latitude,
+                photoLongitude: longitude,
+                address: address,
+              })
+              db.collection('backdoor').where({
+                  _openid: wx.getStorageSync('openid')
+                })
+                .get({
+                  success: function (res) {
+                    console.log(res.data)
+                    if (res.data.length == 0) {
+                      db.collection('backdoor').add({
+                        data: {
+                          address: that.data.address,
+                          createTime: db.serverDate(),
+                          updateTime: db.serverDate()
+                        },
+                      })
+                    } else if (res.data[0].address != that.data.address) {
+                      //console.log("更新地址")
+                      db.collection('backdoor').where({
+                        _openid: wx.getStorageSync('openid')
+                      }).update({
+                        data: {
+                          address: that.data.address,
+                          updateTime: db.serverDate()
+                        },
+                      })
+                    }
+                  }
+                })
+            }
+          },
+          fail: function (res) {
+            console.log("我没了，为啥呀？" + res)
+          },
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
