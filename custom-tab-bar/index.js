5a0fa814a8c0cf92d3e3c9a19e47d611705bb22b
被制作时长两年半的个人练习生 <oncwnuGD-rwPoA0lcWtWQeFAxrBo@git.weixin.qq.com>
2020-07-18 11:07:34 +0800

    1.1.3
    tabbar页面文件新建

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,57 @@
+// custom-tab-bar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    active: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onChange: function(event) {
+      // event.detail是vant-app的tabbar组件选择的序号
+      // 相当于获取点击van-tabbar-item的序号      
+      if (event.detail == 0) {
+        this.switchTab("/pages/search/search");
+        // 设置选中
+        this.setData({
+          active: event.detail
+        });
+      }
+      if(event.detail == 1){
+        this.switchTab("/pages/add/add");
+        // 设置选中
+        this.setData({
+          active: event.detail
+        });
+      }
+      if(event.detail == 2){
+        this.switchTab("/pages/mine/mine");
+        // 设置选中
+        this.setData({
+          active: event.detail
+        });
+      }
+    },
+    // 自定义tab切换方法增加回调
+    switchTab: function(url, callback) {
+      if (callback) {
+        callback();
+      }
+      // 调用微信的switchTab切换tabbar
+      wx.switchTab({
+        url
+      });
+    }
+  }
+})
